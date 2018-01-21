853ccabbebc4923510653d2b9de6aedaef767ab1
theo9921 <33980928+theo9921@users.noreply.github.com>
2018-01-21 01:52:58 +0000

    Updated TestCV to detect faces on webcam window

--- face_detection/testCV.py
+++ face_detection/testCV.py
@@ -1,53 +1,37 @@
-'''
-
-Simply display the contents of the webcam with optional mirroring using OpenCV 
-
-via the new Pythonic cv2 interface.  Press <esc> to quit.
-
-'''
-import os
-import numpy as np
 import cv2
 
-def detect_face(img):
-    #convert the test image to gray image as opencv face detector expects gray images
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    #load OpenCV face detector, I am using LBP which is fast
-    #there is also a more accurate but slow Haar classifier
-    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
-
-    #let's detect multiscale (some images may be closer to camera than others) images
-    #result is a list of faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
-    
-    #if no faces are detected then return original img
-    if (len(faces) == 0):
-        return None, None
-    
-    #under the assumption that there will be only one face,
-    #extract the face area
-    (x, y, w, h) = faces[0]
-    
-    #return only the face part of the image
-    return gray[y:y+w, x:x+h], faces[0]
+cap = cv2.VideoCapture(0)
+cap.set(3, 640) #WIDTH
+cap.set(4, 480) #HEIGHT
 
 
 
-def show_webcam(mirror=False):
-    cam = cv2.VideoCapture(0)
-    while True:
-        ret_val, img = cam.read()
-        if mirror:
-            img=cv2.flip(img,1)
+face_cascade = cv2.CascadeClassifier('C:\\Users\\Theodore\\Anaconda3\\pkgs\\opencv-3.3.1-py36h20b85fd_1\\Library\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+
+
+while(True):
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    print(len(faces))
+    # Display the resulting frame
+
+    for (x,y,w,h) in faces:
         
-        cv2.imshow('my webcam', img)
-        if cv2.waitKey(1)==27:
-            break #esc to quit
-    detect_face(img)    
-    cv2.destroyAllWindows()
-    
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+         roi_gray = gray[y:y+h, x:x+w]
+         roi_color = frame[y:y+h, x:x+w]
 
-show_webcam(mirror=False)
-    
-    
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1)==27:
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
